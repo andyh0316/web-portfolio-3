74e451958ed6c3dd2993d5423cc86c7413b0c72e
andyh0316 <andyhong0316@yahoo.com>
2021-08-13 13:26:24 -0700

    push

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -12,14 +12,11 @@ export default class VideoBackground extends Component {
       <div className="video-background">
         <div className="video-foreground">
           <iframe
-            src={"https://www.youtube.com/embed/" + this.props.youtubeId + "?playlist=" + this.props.youtubeId + "&loop=1&mute=1&autoplay=1&controls=0&showinfo=0&autohide=1"}
+            src={"https://www.youtube.com/embed/" + this.props.youtubeId + "?playlist=" + this.props.youtubeId + "&loop=1&autoplay=1&mute=1&controls=0&showinfo=0&autohide=1"}
             style={{ position: "relative", width: "100%", height: "100vh", top: "0px", left: "0px" }}
-            frameborder="0"
-            webkitallowfullscreen=""
-            mozallowfullscreen=""
-            allowfullscreen=""
+            frameBorder="0"
+            allowFullScreen=""
             allow="autoplay; fullscreen; encrypted-media"
-            class="fade-out"
           ></iframe>
         </div>
       </div>
